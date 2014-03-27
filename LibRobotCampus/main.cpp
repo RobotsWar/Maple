a349e917902df46266346bba93c8a7ba0437e169
Gregwar <g.passault@gmail.com>
2014-03-27 10:59:22 +0100

    Removing auto init

--- LibRobotCampus/main.cpp
+++ LibRobotCampus/main.cpp
@@ -3,70 +3,7 @@
 #include <servos.h>
 #include <terminal.h>
 #include <main.h>
-#include <dxl.h>
-#include "wifly.h"
-
-volatile bool flag = false;
-volatile bool isUSB = false;
-
-/**
- * Interruption @50hz
- */
-static void setFlag()
-{
-    // Ne pas écrire de code ici à moins de vraiment
-    // savoir ce que vous faites
-    flag = true;
-}
-
-/**
- * Setting up the board
- */
-static void internal_setup()
-{
-    // Intialise les servomoteurs
-    servos_init();
-    
-    // Initialise le mode WiFly
-    WiFly.begin(921600);
-    terminal_init(&WiFly);
-
-    // Configure la led de la board
-    pinMode(BOARD_LED_PIN, OUTPUT);
-    digitalWrite(BOARD_LED_PIN, LOW);
-
-    // Lance la configuration de l'utilisateur
-    setup();
-    
-    // Définit l'interruption @50hz
-    servos_attach_interrupt(setFlag);
-}
-
-/**
- * Boucle principale
- */
-static void internal_loop()
-{
-    // Gère la communication du terminal
-    terminal_tick();
-
-    // Si quelque chose est disponible au niveau de l'USB, switch
-    // sur le port
-    if (SerialUSB.available() && !isUSB) {
-        isUSB = true;
-        terminal_init(&SerialUSB);
-    }
-
-    // Exécute le code @50hz
-    if (flag) {
-        flag = false;
-        tick();
-    }
-
-    // Appelle la fonction loop de l'utilisateur
-    loop();
-}
-
+ 
 // Force init to be called *first*, i.e. before static object allocation.
 // Otherwise, statically allocated objects that need libmaple may fail.
 __attribute__((constructor)) void premain()
@@ -76,10 +13,10 @@ __attribute__((constructor)) void premain()
 
 int main(void)
 {
-    internal_setup();
+    setup();
 
     while (true) {
-        internal_loop();
+        loop();
     }
 
     return 0;
--- skeleton/main.cpp
+++ skeleton/main.cpp
@@ -4,28 +4,20 @@
 #include <terminal.h>
 #include <main.h>
 
-TERMINAL_PARAMETER_DOUBLE(t, "Temps", 0.0);
+TERMINAL_COMMAND(hello, "Prints hello world")
+{
+    terminal_io()->println("Hello world");
+}
 
 /**
- * Vous pouvez écrire du code qui sera exécuté à 
- * l'initialisation ici
+ * Setup function
  */
 void setup()
 {
 }
 
 /**
- * Foncton appellée à 50hz, c'est ici que vous pouvez mettre
- * à jour les angles moteurs etc.
- */
-void tick()
-{
-    t += 0.02; // 20ms
-}
-
-/**
- * Si vous souhaitez écrire ici du code, cette fonction sera
- * apellée en boucle
+ * Loop function
  */
 void loop()
 {
